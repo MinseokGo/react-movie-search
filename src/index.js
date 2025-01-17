03262b915d5d8bff943b11d606d4341f2b54a74c
MinseokGo <rhalstjr1999@naver.com>
2025-01-18 00:02:48 +0900

    init: 프로젝트 세팅

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SearchBar from './_components/SearchBar/searchBar';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SearchBar />
+  </React.StrictMode>
+);
