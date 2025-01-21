2350a7622e422dad902554bb7f9f8a5fd122bff7
MinseokGo <rhalstjr1999@naver.com>
2025-01-21 18:43:25 +0900

    refactor: 최상위 컴포넌트 변경

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SearchBar from './_components/SearchBar/searchBar';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <SearchBar />
+    <App />
   </React.StrictMode>
 );
